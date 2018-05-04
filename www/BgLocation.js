e4190bb18c760b46bc48646b1aa297e5dd79dd70
Ingeneo sas <ingeneo@MacBook-Air-de-Ingeneo-2.local>
2018-05-04 09:05:02 -0500

    Correccion nombre del metodo

--- www/BgLocation.js
+++ www/BgLocation.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
 exports.initLocation = function (token, userId, device, url, schedule, distance, time, success, error) {
-    exec(success, error, 'BgLocation', 'coolMethod', [token, userId, device, url, schedule, distance, time]);
+    exec(success, error, 'BgLocation', 'initLocation', [token, userId, device, url, schedule, distance, time]);
 };
